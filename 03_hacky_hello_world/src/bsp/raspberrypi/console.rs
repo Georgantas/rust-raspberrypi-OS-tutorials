31e83766fbd9a4677758344c166250b897bd5dcf
John Georgantas <github@georgantas.me>
2021-02-07 08:41:31 -0800

    Complete lesson 3.

--- 03_hacky_hello_world/src/bsp/raspberrypi/console.rs
+++ 03_hacky_hello_world/src/bsp/raspberrypi/console.rs
@@ -29,6 +29,7 @@ impl fmt::Write for QEMUOutput {
     fn write_str(&mut self, s: &str) -> fmt::Result {
         for c in s.chars() {
             unsafe {
+                /// 0x3F20_1000 is the address for the serial interface on the rpi3
                 core::ptr::write_volatile(0x3F20_1000 as *mut u8, c as u8);
             }
         }
